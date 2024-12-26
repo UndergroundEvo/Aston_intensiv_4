3d5a6b6fbc7d92f2797b50bf941f682f37179f0c
Miron <popovmiron@gmail.com>
2024-12-26 19:36:35 +0700

    Data класс пользователя и строки

--- app/src/main/java/ru/aston/fragmentnav/User.kt
+++ app/src/main/java/ru/aston/fragmentnav/User.kt
@@ -1,9 +1,13 @@
 package ru.aston.fragmentnav
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class User(
     var id: Int,
     var firstName: String,
     var lastName: String,
     var phoneNumber: String,
     var photoUrl: String
-)
+) : Parcelable
